11ea2ed85c14da2109202dfbebeb6e7dad873684
Mikolaj Rdzanek <mikolaj.rdzanek@sick.de>
2021-07-20 15:24:47 +0200

    Initial commit

---
+++ CerealClassifier_DeepNeuralNetwork/scripts/main.lua
@@ -0,0 +1,71 @@
+--Start of Global Scope---------------------------------------------------------
+
+print('AppEngine Version: ' .. Engine.getVersion())
+
+local DELAY = 1000 -- ms between visualization steps for demonstration purpose
+
+-- Create a viewer
+local viewer = View.create()
+
+-- Load a network from resources
+local net = Object.load('resources/cereal_net.json') -- This will be a MachineLearning.DeepNeuralNetwork.Model instance
+print(MachineLearning.DeepNeuralNetwork.Model.toString(net))
+
+-- Create a inference engine instance
+local dnn = MachineLearning.DeepNeuralNetwork.create()
+
+-- Create a text decoration for the presentation in the viewer
+local textDeco = View.TextDecoration.create()
+textDeco:setSize(8)
+textDeco:setPosition(3, 8)
+textDeco:setColor(0, 255, 0)
+--End of Global Scope-----------------------------------------------------------
+
+--Start of Function and Event Scope---------------------------------------------
+
+-- Load the images in the resources/images folder and return an array
+local function loadImages()
+  local flakeImage = Image.load('resources/images/corn_flakes.png')
+  local puffImage = Image.load('resources/images/oat_squares.png')
+  local branImage = Image.load('resources/images/bran_flakes.png')
+  return {flakeImage, puffImage, branImage}
+end
+
+-- Show the image in the viewer and also display the predicted label and the confidence of the prediction
+local function showResultInViewer(image, label, confidence)
+  viewer:clear()
+  viewer:addImage(image)
+  viewer:addText(
+    string.format('Class: %s,\nConfidence: %0.1f%%', label, confidence * 100),
+    textDeco
+  )
+  viewer:present()
+end
+
+local function main()
+  dnn:setModel(net) -- Load the network into the engine
+  local image_list = loadImages() -- Load the images to give to the network
+  for img_nr, image in ipairs(image_list) do
+    dnn:setInputImage(image) -- Load the image for the network
+    local result = dnn:predict() -- Run the network on the image
+
+    -- Parse the result as a classification output
+    local predicted_class_idx, pred_confidence, pred_label = result:getAsClassification()
+
+    -- Print the result to the console
+    print(
+      string.format(
+        'Image %i was classified as %i: %s with %.01f %% confidence',
+        img_nr, predicted_class_idx, pred_label, pred_confidence * 100
+      )
+    )
+
+    -- Send the result to the viewer
+    showResultInViewer(image, pred_label, pred_confidence)
+    Script.sleep(DELAY)
+  end
+  print('App finished.')
+end
+Script.register('Engine.OnStarted', main)
+
+--End of Function and Event Scope--------------------------------------------------
---
+++ readme.md
@@ -0,0 +1,14 @@
+## CerealClassifier_DeepNeuralNetwork
+Loading a trained neural network and running predictions on locally saved images.
+
+### How to Run
+Starting this sample is possible either by running the app (F5) or
+debugging (F7+F10). Setting breakpoint on the first row inside the 'main'
+function allows debugging step-by-step after 'Engine.OnStarted' event.
+Results can be seen in the image viewer on the DevicePage.
+Restarting the Sample may be necessary to show images after loading the web-page.
+To run this Sample a device with SICK Algorithm API and AppEngine >= V2.11.0 is
+required.
+
+### Topics
+Algorithm, Image-2D, Machine-Learning, Sample, SICK-AppSpace
